fc648b0961c92ac5f2ee2a96117678241ad7fc6e
imabid141 <imabid141@gmail.com>
2026-02-07 14:28:16 +0500

    feat: demonstrate usage of System, Human, and AI message objects

---
+++ messages.py
@@ -0,0 +1,25 @@
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.3-70B-Instruct",
+    task="text-generation",
+    max_new_tokens=512,
+    temperature=0.7
+)
+
+model = ChatHuggingFace(llm=llm)
+
+messages=[
+    SystemMessage(content='You are a helpful assistant'),
+    HumanMessage(content='Tell me about LangChain')
+]
+
+result = model.invoke(messages)
+
+messages.append(AIMessage(content=result.content))
+
+print(messages)
