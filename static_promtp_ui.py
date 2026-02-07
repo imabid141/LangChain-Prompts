ce463f6004e94338137c5d64e13647d2afffd687
imabid141 <imabid141@gmail.com>
2026-02-07 14:43:48 +0500

    feat: add static UI for research summarization

---
+++ static_promtp_ui.py
@@ -0,0 +1,21 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+import streamlit as st
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen3-Coder-Next",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+st.header ('Research Tool')
+
+user_input = st.text_input('Enter your prompt')
+
+if st.button('Summarize'):
+    result = model.invoke(user_input)
+    st.write(result.content)
+    
